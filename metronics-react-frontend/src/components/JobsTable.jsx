8dc947619a1e5ac204b49c384e5b256440c4f084
Jordan Kelly <profjjk@gmail.com>
2021-08-05 15:41:21 -0700

    add search functionality to Jobs page

--- metronics-react-frontend/src/components/JobsTable.jsx
+++ metronics-react-frontend/src/components/JobsTable.jsx
@@ -2,44 +2,47 @@ import { useEffect, useState } from 'react';
 
 const JobsTable = ({ setShowForm, setJobId, searchTerm, jobs }) => {
   const [jobList, setJobList] = useState(jobs)
-  const [jobFilter, setJobFilter] = useState(jobList);
   const [jobStatus, setJobStatus] = useState("");
 
-
+  // Filter by status
   useEffect(() => {
     if (jobStatus === "Waiting") {
-      setJobFilter(jobList.filter((job) => job.status === "waiting"));
+      setJobList(jobList.filter(job => job.status === "waiting"));
     } else if (jobStatus === "Scheduled") {
-      setJobFilter(jobList.filter((job) => job.status === "scheduled"));
+      setJobList(jobList.filter(job => job.status === "scheduled"));
     } else if (jobStatus === "Completed") {
-      setJobFilter(jobList.filter((job) => job.status === "completed"));
+      setJobList(jobList.filter(job => job.status === "completed"));
     } else if (jobStatus === "Canceled") {
-      setJobFilter(jobList.filter((job) => job.status === "canceled"));
+      setJobList(jobList.filter(job => job.status === "canceled"));
     } else {
-      setJobFilter(jobList);
+      setJobList(jobs);
     }
   }, [jobStatus]);
 
+  // Search for jobs
   useEffect(() => {
     if (searchTerm === '') {
       setJobList(jobs);
       return;
     }
-    const searchResults = [];
-    for (let job of jobList) {
-      let name = job.customer.businessName.toLowerCase(); let invoice = job.invoiceNumber; let date = job.dateCompleted;
-      if (name.indexOf(searchTerm) !== -1 || invoice.indexOf(searchTerm) !== -1 || date.indexOf(searchTerm) !== -1) {
-        searchResults.push(job);
+    setJobList(jobs.filter(job => {
+      if (job.customer.businessName.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return true;
+      } else if (job.dateCompleted !== null && job.dateCompleted.includes(searchTerm)) {
+        return true;
+      } else if (job.invoiceNumber !== null && job.invoiceNumber.includes(searchTerm)) {
+        return true;
+      } else {
+        return false;
       }
-    }
-    setJobList(searchResults);
+    }))
   }, [searchTerm]);
 
+  // Handlers
   const selectionHandler = (e) => {
     e.preventDefault();
     setJobStatus(e.target.value);
   };
-
   const clickHandler = (e) => {
     e.preventDefault();
     setJobId(parseInt(e.target.dataset.id));
@@ -63,15 +66,15 @@ const JobsTable = ({ setShowForm, setJobId, searchTerm, jobs }) => {
           <tr>
             <th scope="col">Invoice #</th>
             <th scope="col">Date Completed</th>
-            <th scope="col">Type</th>
             <th scope="col">Company Name</th>
             <th scope="col">City</th>
+            <th scope="col">Type</th>
             <th scope="col">Status</th>
             <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
-          {jobFilter.map(job => (
+          {jobList.map(job => (
             <tr key={job.id}>
               <td className="text-center">
                 {job.invoiceNumber ? job.invoiceNumber : "--"}
