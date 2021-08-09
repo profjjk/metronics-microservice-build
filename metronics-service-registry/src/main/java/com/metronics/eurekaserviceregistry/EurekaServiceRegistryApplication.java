03028d3533bbab1d21c045de2ddb911843bec2da
Jordan Kelly <profjjk@gmail.com>
2021-08-09 09:23:44 -0700

    update package name

--- metronics-service-registry/src/main/java/com/metronics/eurekaserviceregistry/EurekaServiceRegistryApplication.java
+++ metronics-service-registry/src/main/java/com/metronics/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.trilogyed.eurekaserviceregistry;
+package com.metronics.eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- metronics-service-registry/src/test/java/com/metronics/eurekaserviceregistry/EurekaServiceRegistryApplicationTests.java
+++ metronics-service-registry/src/test/java/com/metronics/eurekaserviceregistry/EurekaServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.trilogyed.eurekaserviceregistry;
+package com.metronics.eurekaserviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
